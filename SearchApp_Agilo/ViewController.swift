b41dbc49e7f277e6abdd7986abca8a51b4584765
Chetankumar Wakude <c.wakude@mobily.com.sa>
2020-06-19 12:34:19 +0530

    Initial Commit

---
+++ SearchApp_Agilo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchApp_Agilo
+//
+//  Created by Chetan on 19/06/20.
+//  Copyright © 2020 Chetan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
